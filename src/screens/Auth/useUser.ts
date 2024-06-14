d60cd71ae36285a448f88884341b4f29f5c3c1eb
MahdiarShirzad <101416335+MahdiarShirzad@users.noreply.github.com>
2024-06-15 01:01:27 +0330

    .

--- src/screens/Auth/useUser.ts
+++ src/screens/Auth/useUser.ts
@@ -1,17 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../services/apiAuth";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "../../features/userSlice";
+import { useSelector } from "react-redux";
+// import { setUser } from "../../features/userSlice";
 import { RootState } from "../../features/store";
 
 export function useUser() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const { isLoading } = useQuery({
     queryKey: ["user"],
     queryFn: getCurrentUser,
-    onSuccess: (data: any) => {
-      dispatch(setUser(data));
-    },
+    // onSuccess: (data: any) => {
+    //   dispatch(setUser(data));
+    // },
   });
 
   const userState = useSelector((state: RootState) => state.user);
